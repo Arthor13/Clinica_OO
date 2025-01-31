036ec52a27e378d5fab014d6984868d368b19837
Arthur de Lima Sobreira <112632734+Arthor13@users.noreply.github.com>
2025-01-31 14:25:17 -0300

    Create Pessoa.java
    Primeiro commit do projeto e criação da classe Pessoa.

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,42 @@
+package model;
+
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+
+    // Construtor
+    public Pessoa(String nome, String cpf, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento; 
+
+    // Getter e Setter para nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter e Setter para CPF
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    // Getter e Setter para Data de Nascimento
+    public String getData() {
+        return dataNascimento;
+    }
+
+    public void setData(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
