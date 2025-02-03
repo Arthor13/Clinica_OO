914b3757c13f23f200cb082deaa631973dc69fc3
EduardoRibeiroXavier <81425206+EduardoRibeiroXavier@users.noreply.github.com>
2025-02-03 13:08:21 -0300

    Criação Classe Notificacao

---
+++ src/main/java/Notificacao.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class Notificacao {
+    private String mensagem;
+    private LocalDateTime dataNotificacao;
+    private String destinatario;
+    private TipoNotificacao tipo;
+
+    public Notificacao(String mensagem, String destinatario, TipoNotificacao tipo) {
+        this.mensagem = mensagem;
+        this.dataNotificacao = LocalDateTime.now();
+        this.destinatario = destinatario;
+        this.tipo = tipo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataNotificacao() {
+        return dataNotificacao;
+    }
+
+    public void setDataNotificacao(LocalDateTime dataNotificacao) {
+        this.dataNotificacao = dataNotificacao;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public TipoNotificacao getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoNotificacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public void enviarNotificacao(){
+        System.out.println("Notificação para: " + destinatario);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Mensagem: "+ mensagem);
+        System.out.println("Data/Hora: "+ dataNotificacao);
+        System.out.println("-------------------------------------------------------");
+    }
+}
---
+++ src/main/java/TipoNotificacao.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum TipoNotificacao {
+    CONSULTA_AGENDADA,
+    PAGAMENTO_PENDENTE,
+    EXAME_AGENDADO,
+    LEMBRETE_CONSULTA;
+}
