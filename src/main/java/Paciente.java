22be954282813855c85b097dd0f13d097dde90c0
Arthur de Lima Sobreira <112632734+Arthor13@users.noreply.github.com>
2025-02-05 08:51:24 -0300

    add classes paciente and medico

---
+++ src/main/java/Paciente.java
@@ -0,0 +1,33 @@
+package model;
+
+class Paciente extends Pessoa {
+
+    private List<String> historicoExames;
+    private boolean pagamentoPendente;
+
+    public Paciente(String nome, String cpf, LocalDate dataNascimento) {
+        super(nome, cpf, dataNascimento);
+        this.historicoExames = new ArrayList<>();
+        this.pagamentoPendente = false;
+    }
+
+    public List<String> getHistoricoExames() {
+        return historicoExames;
+    }
+
+    public void adicionarExame(String exame) {
+        historicoExames.add(exame);
+    }
+
+    public boolean isPagamentoPendente() {
+        return pagamentoPendente;
+    }
+
+    public void bloquearCadastro() {
+        this.pagamentoPendente = true;
+    }
+
+    public void realizarPagamento() {
+        this.pagamentoPendente = false;
+    }
+}
