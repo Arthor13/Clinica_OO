d0700ee99ea023a94b766fa7a549c9101a61e010
Arthur de Lima Sobreira <112632734+Arthor13@users.noreply.github.com>
2025-02-14 20:15:51 -0300

    codigo desenvolvido pelo integrante Arthur Souto, fazendo o commit pela minha conta tendo em vista que o integrante nao conseguiu dar commit da propria maquina

---
+++ src/main/java/servicos/ServicoPrescricao.java
@@ -0,0 +1,32 @@
+
+public class ServicoPrescricao {
+    private List<Prescricao> prescricoes = new ArrayList<>();
+    
+    // Create
+    public void adicionarPrescricao(Prescricao prescricao) {
+        prescricoes.add(prescricao);
+    }
+
+    // Read
+    public List<Prescricao> listarPrescricoes() {
+        return prescricoes;
+    }
+
+    // Update
+    public void atualizarPrescricao(int index, Prescricao prescricaoAtualizada) {
+        if (index >= 0 && index < prescricoes.size()) {
+            prescricoes.set(index, prescricaoAtualizada);
+        } else {
+            System.out.println("Prescrição não encontrada.");
+        }
+    }
+
+    // Delete
+    public void removerPrescricao(int index) {
+        if (index >= 0 && index < prescricoes.size()) {
+            prescricoes.remove(index);
+        } else {
+            System.out.println("Prescrição não encontrada.");
+        }
+    }
+}
